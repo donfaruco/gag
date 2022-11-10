779e0800f985baf418709ecbb0681ace19d61111
donfaruco <donfaruco@gmail.com>
2022-11-10 15:23:22 -0800

    Update trustinfo.py

--- trustinfo.py
+++ trustinfo.py
@@ -1,22 +1,31 @@
 """Gag script to provide trust reference to a friend."""
 
-import os
+import webbrowser
+import sys
+
+
+city = sys.argv[1]
+url = "https://savinbursklaw.com/"
+directions = "https://www.google.com/maps/dir/Woodley+Ave+%26+Rinaldi+St,+Los+Angeles,+CA+91344/10663+Yarmouth+Ave,+Granada+Hills,+CA+91344/@34.2718048,-118.5208106,14z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x80c2900992118cb1:0x33e7de3cd8afaf17!2m2!1d-118.4846721!2d34.27918!1m5!1m1!1s0x80c29add568c4543:0x98e7417a1b27d322!2m2!1d-118.5219299!2d34.2641603"
 
 def mytrust(city):
-    office = "Law offices of Savin & Bursk"
-    phone = "818-368-8646"
-    address = "10663 Yarmouth Ave"
-    zip = "91344"
+    data = """
+    Name: Law offices of Savin & Bursk
+    Phone: 818-368-8646
+    Address = 10663 Yarmouth Ave.
+    Granada Hills, CA. 91344
+    """
 
     if city != "Granada Hills":
         return "incorrect city, please enter Granada Hills"
+        exit()
     else:
-        return office, phone, address, city, zip
+        return data
 
-trust = mytrust("Granada Hills")
+trust = mytrust(city)
 
-print("Name:" + trust[0])
+print(trust)
 
-print("contact:" + trust[1])
-print("address:" + trust[2])
-print(trust[3] +", " + trust[4])
+webbrowser.open_new(url)
+webbrowser.open_new(directions)
+
