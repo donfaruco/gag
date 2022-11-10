7ca1f054a004d1ed4ec8f6693f7b07f1c5197a73
Don Faruco <donfaruco@doni27.home>
2022-11-10 11:37:30 -0800

    adding script

---
+++ trustinfo.py
@@ -0,0 +1,14 @@
+"""Gag script to provide trust reference to a friend."""
+def mytrust(city):
+    office = "Law offices of Savin & Bursk"
+    phone = "818-368-8646"
+    address = "10663 Yarmouth Ave"
+    zip = "91344"
+
+    if city != "Granada Hills":
+        return "incorrect city, please enter Granada Hills"
+    else:
+        return office, phone, address, city, zip
+
+trust = mytrust("Granada Hills")
+print(trust)
