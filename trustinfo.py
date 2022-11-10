7adae7b89720bf6faeafae98c7f986415d0a1664
donfaruco <donfaruco@gmail.com>
2022-11-10 13:22:25 -0800

    Update trustinfo.py

--- trustinfo.py
+++ trustinfo.py
@@ -1,5 +1,7 @@
 """Gag script to provide trust reference to a friend."""
 
+import os
+
 def mytrust(city):
     office = "Law offices of Savin & Bursk"
     phone = "818-368-8646"
@@ -13,4 +15,8 @@ def mytrust(city):
 
 trust = mytrust("Granada Hills")
 
-print(trust)
+print("Name:" + trust[0])
+
+print("contact:" + trust[1])
+print("address:" + trust[2])
+print(trust[3] +", " + trust[4])
